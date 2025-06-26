1fa50aea53bde650dd7b2b6e3861b4b0c120db32
JsCodexx <muhammadnomi425@gmail.com>
2025-06-26 15:51:57 +0500

    added 404 image

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -3,15 +3,11 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="not-found-page flex flex-col items-center justify-center min-h-[90vh] bg-white dark:bg-black text-center px-4">
-      <h1 className="text-6xl font-bold text-red-600 mb-4">404</h1>
-      <p className="text-xl text-gray-700 dark:text-gray-300 mb-6">
-        {" Oops! The page you're looking for doesn't exist."}
-      </p>
+    <div className="not-found-page flex flex-col items-center justify-center min-h-[90vh] bg-red-200 dark:bg-black text-center px-4">
       <img
-        src="/images/404.jpg"
+        src="/images/404.png"
         alt="404 Not Found"
-        className="w-64 h-auto mb-6"
+        className="w-[50%] h-[400px] mb-6"
       />
 
       <Link href="/" className="text-red-600 hover:underline">
